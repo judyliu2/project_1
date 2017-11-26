8f42070000a749c63f083754a8068e9e08d96c1b
judyliu2 <jliu15@stuy.edu>
2017-11-26 16:19:49 -0500

    added parent/child, ls stil doesn't work

--- makefile
+++ makefile
@@ -1,15 +1,16 @@
-tester: methods.o shell.o
-	@gcc methods.o shell.o -o tester
+all: methods.o shell.o
+	@gcc -o tester methods.o shell.o
 
-run: tester
-	@./tester
 
 methods.o: methods.c shell.h
 	@gcc -c methods.c
 
-shell.o: shell.c shell.h
+shell.o: shell.c shell.h 
 	@gcc -c shell.c
 
 clean:
 	@rm *.o
 	@rm *~
+
+run: all
+	@./tester
--- shell.c
+++ shell.c
@@ -3,25 +3,41 @@
 #include "shell.h"
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 int main()
 {
   char dest[256];
-  int f;
 
   printf("Enter command: ");
   fgets(dest, sizeof(dest), stdin);
-
+  //printf("%s",dest);
   char commands[256];
   strcpy(commands,dest);
   char ** test = parse_args(commands);
-  execvp(test[0], test);
+  
+  int child = fork();
+   if (!child){
+    
+     execvp(test[0], test);
+   }
+   else{
+     //printf("Enter command: ");
+     fgets(dest, sizeof(dest), stdin);
+     //printf("%s",dest);
+     char commands[256];
+     strcpy(commands,dest);
+     char ** test = parse_args(commands);
+     child = fork();
+     
+     if(!child){
+       execvp(test[0], test);
+     }
 
-  /*
-  f = fork();
-
-  if (f) {
-  }
-  */
+     int status;
+     int finished = wait(&status);
+     int ret = WEXITSTATUS(status);
+   }
+   
   return 0;
 }
