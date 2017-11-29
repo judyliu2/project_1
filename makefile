067e4563fd8c3a4cf7267b15c2b1e87afd3429c5
Gian Tricarico <gtricarico@stuy.edu>
2017-11-29 17:02:02 -0500

    get semicolons to work, but only with no spaces

--- makefile
+++ makefile
@@ -8,6 +8,18 @@ methods.o: methods.c shell.h
 shell.o: shell.c shell.h 
 	@gcc -c shell.c
 
+shell_debug: methods_debug.o shell_debug.o
+	@gcc -o shell_debug methods_debug.o shell_debug.o
+
+methods_debug.o: methods.c shell.h
+	@gcc -g -c methods.c -o methods_debug.o
+
+shell_debug.o: shell.c shell.h
+	@gcc -g -c shell.c -o shell_debug.o
+
+debug: shell_debug
+	valgrind --leak-check=yes ./shell_debug
+
 clean:
 	@rm *.o
 	@rm *~
--- shell.c
+++ shell.c
@@ -10,8 +10,8 @@ int main()
 
   int f = 1;
   char ** commands = (char**) calloc(20, sizeof(char*));
-  char *  dest = (char)calloc(100, sizeof(char));
-  char ** line = (char *)calloc(20, sizeof(char*));
+  char *  dest = (char*)calloc(100, sizeof(char));
+  char ** line = (char **)calloc(20, sizeof(char*));
   int i;
 
   while (f) {
@@ -20,11 +20,17 @@ int main()
     
     dest[strlen(dest) -1] = 0; //gets rid of the new line at the end of the parsed array
 
-    commands = parse_args(dest);
-    printf("%d\n", commands[1]);
+    // printf("%d\n", commands[1]);
     
     line = parse_argC(dest); //separated by semicolons;
-
+    while (f && line[0]) {
+      printf("%d\n", line);
+      // commands = parse_args(dest);
+      printf("[%s]\n", line[0]);
+      commands = parse_args(line[0]);
+      // printf("%d\n", parse_args(*line));
+      printf("[%s]\n", commands[0]);
+    
     // int num_of_commands = sizeof(commands)/sizeof(char*);
     // for (i = 0; line[i]
     
@@ -32,23 +38,26 @@ int main()
     //command = parse_args(command[num_of_commands]); //separated by spaces
     
     
-    if (!strncmp(commands[0], "exit", 4)) {
-      break;
+    if (strncmp(commands[0], "exit", 4) == 0) {
+      return 0;
     }
     //else 
-    if (!strncmp(commands[0], "cd", 2)) {
+    if (strncmp(commands[0], "cd", 2) == 0) {
       chdir(commands[1]);
     }
-//else {
+    else {
     
-      f = fork();
+    f = fork();
     
-      if (f){
-	int status;
-	int finish = wait(&status);
-	  }
+    if (f){
+      int status;
+      int finish = wait(&status);
+    }
+    }
+    line++;
+    } // end inner while loop
       //}
-  } // end while loop
+  } // end outer while loop
 
   // Only child will get to this part:
   
