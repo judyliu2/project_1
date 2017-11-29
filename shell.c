ad2333e80cbc8f10814c939eee6c15906d40e1a1
Gian Tricarico <gtricarico@stuy.edu>
2017-11-29 11:46:46 -0500

    make it keep running after one command

--- shell.c
+++ shell.c
@@ -7,38 +7,53 @@
 
 int main()
 {
-  
-    char *  dest= (char *) calloc(20, sizeof(char*));
+
+  int f = 1;
+  char ** commands = (char**) calloc(20, sizeof(char*));
+
+  while (f) {
+    char *  dest = (char *) calloc(20, sizeof(char*));
     printf("Enter commands: ");
     fgets(dest, 30, stdin);
     
     dest[strlen(dest) -1] = 0; //gets rid of the new line at the end of the parsed array
-    char ** commands = (char**) calloc(20, sizeof(char*));
-    commands = parse_args(dest);
-    // commands = parse_argC(dest); //separated by semicolons;
 
-    //int num_of_commands = 0;
-    //num_of_commands = sizeof(commands)/sizeof(char*);
+    commands = parse_args(dest);
+
+    line = parse_argC(dest); //separated by semicolons;
+
+    int num_of_commands = 0;
+    num_of_commands = sizeof(commands)/sizeof(char*);
     
-    //while(num_of_commands){
-    //char ** command = (char**) calloc(20,sizeof(char*));
+   
     //command = parse_args(command[num_of_commands]); //separated by spaces
     
-    //make this into a loop
-    //while(command){
-    int parent = getpid();
-    int child = fork();
     
-    if (getpid() == parent){
-      int status;
-      int finish = wait(&status);
+    if (!strncmp(commands[0], "exit", 4)) {
+      break;
     }
-    else{
-      execvp(commands[0], commands);
+    //else 
+    if (!strncmp(commands[0], "cd", 2)) {
+      chdir(commands[1]);
+    }
+//else {
+    
+      f = fork();
+    
+      if (f){
+	int status;
+	int finish = wait(&status);
+	  }
+      //}
+  } // end while loop
+
+  // Only child will get to this part:
+  
+  // else{
+  execvp(commands[0], commands);
       //execvp(command[0],command);
       // return getpid();
-    }
-    //}
+      // }
  
   return 0;
 }
