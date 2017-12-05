a83078437518b190872fcef748a5e781fc630031
Gian Tricarico <gtricarico@stuy.edu>
2017-12-04 21:31:35 -0500

    Add comments

--- methods.c
+++ methods.c
@@ -3,11 +3,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*
+ * Function: parse_args
+ * --------------------
+ * separates a string into an array for use with execvp
+ *
+ *  s1: a string
+ *
+ *  returns: Array of strings where each entry is a token separated by spaces
+ *
+ *  Precondition: s1 is null-terminating and does not exceed 799 tokens
+ *  separated by spaces.
+ *
+ *  Postcondition: replaces each space in s1 with a null character
+ */
 char ** parse_args(char * s1){
 	char ** ret = (char **) calloc (800, sizeof(char*));
 	int i = 0;    
 
-	while (s1 && i < 9){
+	while (s1 && i < 799){
 	  char * string = strsep (&s1," ");
 	  ret[i] = string;
 	  i++;
@@ -16,11 +30,26 @@ char ** parse_args(char * s1){
 	return ret;
 }
 
+/*
+ * Function: parse_argC
+ * --------------------
+ * Separates a string into an array using semicolon as the delimiter
+ *
+ *  s1: a string
+ *
+ *  returns: Array of strings where each entry is a token separated by
+ *           semicolons
+ *
+ *  Precondition: s1 is null-terminating and does not exceed 799 tokens
+ *  separated by semicolons.
+ *
+ *  Postcondition: replaces each semicolon in s1 with a null character
+ */
 char ** parse_argC(char * s1){
 	char ** ret = (char **) calloc (800, sizeof(char*));
 	int i = 0;
 
-	while (s1 && i < 9){
+	while (s1 && i < 799){
 	  char * string = strsep (&s1,";");
 	  ret[i] = string;
 	  i++;
@@ -29,7 +58,20 @@ char ** parse_argC(char * s1){
 	return ret;
 }
 
-// Precondition: s must be null-terminating.
+/*
+ * Function: trim
+ * --------------
+ * Removes spaces from the beginning and end of a string
+ *
+ *  s: a string
+ *
+ *  returns: a string with no spaces at the beginning or at the end
+ *
+ *  Precondition: s is null-terminating.
+ *
+ *  Postcondition: replaces the spaces at the end of s (but not at the
+ *                 beginning) with null characters
+ */
 char * trim(char * s)
 {
   char * str;
--- shell.h
+++ shell.h
@@ -1,3 +1,48 @@
+/*
+ * Function: parse_args
+ * --------------------
+ * separates a string into an array for use with execvp
+ *
+ *  s1: a string
+ *
+ *  returns: Array of strings where each entry is a token separated by spaces
+ *
+ *  Precondition: s1 is null-terminating and does not exceed 799 tokens
+ *  separated by spaces.
+ *
+ *  Postcondition: replaces each space in s1 with a null character
+ */
 char ** parse_args(char*);
+
+/*
+ * Function: parse_argC
+ * --------------------
+ * separates a string into an array using semicolon as the delimiter
+ *
+ *  s1: a string
+ *
+ *  returns: Array of strings where each entry is a token separated by
+ *           semicolons
+ *
+ *  Precondition: s1 is null-terminating and does not exceed 799 tokens
+ *  separated by semicolons.
+ *
+ *  Postcondition: replaces each semicolon in s1 with a null character
+ */
 char ** parse_argC(char*);
+
+/*
+ * Function: trim
+ * --------------
+ * Removes spaces from the beginning and end of a string
+ *
+ *  s: a string
+ *
+ *  returns: a string with no spaces at the beginning or at the end
+ *
+ *  Precondition: s is null-terminating.
+ *
+ *  Postcondition: replaces the spaces at the end of s (but not at the
+ *                 beginning) with null characters
+ */
 char * trim(char *);
